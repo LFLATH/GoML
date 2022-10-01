ac4fd991abac7cbabc2f2220b708afa2bc7e7b77
LFLATH <ludwiglechtreck@gmail.com>
2022-10-01 15:00:20 -0400

    Testing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/LFLATH/GoML
 
 go 1.19
+
+require github.com/LFLATH/GoMath v0.0.0-20220926161517-da26478f4a85 // indirect
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	matrix "github.com/LFLATH/GoMath/Matrix"
+)
+
+func main() {
+
+	a := [][]float64{
+		{1, 2, 3},
+		{4, 5, 6},
+	}
+	b := [][]float64{
+		{1, 2, 3},
+		{0, 0, 0},
+		{3, 4, 5},
+	}
+	matrix.Display(a)
+	c, err := matrix.Dot(a, b)
+	if err != nil {
+		panic(err)
+	}
+	matrix.Display(c)
+}
